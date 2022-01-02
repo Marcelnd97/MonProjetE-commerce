72044cc2a8e944223017d86ccead8a62ffb25581
Marcel Nd. Ndiaye <nmarelndiana32@gmail.com>
2022-01-02 13:11:54 +0000

    la suite du projet ecom

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
@@ -45,4 +45,58 @@
             </div>
         </form>
     </x-jet-authentication-card>
+</x-guest-layout> --}}
+
+<x-guest-layout>
+
+    <main id="main" class="main-site left-sidebar">
+
+		<div class="container">
+
+			<div class="wrap-breadcrumb">
+				<ul>
+					<li class="item-link"><a href="#" class="link">Accueil</a></li>
+					<li class="item-link"><span>Connexion</span></li>
+				</ul>
+			</div>
+			<div class="row">
+				<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12 col-md-offset-3">
+					<div class=" main-content-area">
+						<div class="wrap-login-item ">						
+							<div class="login-form form-item form-stl">
+
+                                <x-jet-validation-errors class="mb-4" />
+
+								<form name="frm-login" action="{{route('login')}}" method="POST">
+                                    @csrf
+									<fieldset class="wrap-title">
+										<h3 class="form-title">Connectez-vous à votre compte</h3>										
+									</fieldset>
+									<fieldset class="wrap-input">
+										<label for="frm-login-uname">Address Email:</label>
+										<input type="email" id="frm-login-uname" name="email" placeholder="Tapez votre address email" :value="old('email')" required autofocus>
+									</fieldset>
+									<fieldset class="wrap-input">
+										<label for="frm-login-pass">Mot de passe:</label>
+										<input type="password" id="frm-login-pass" name="password" placeholder="************" required autofocus="current-password">
+									</fieldset>
+									
+									<fieldset class="wrap-input">
+										<label class="remember-field">
+											<input class="frm-input " name="remember" id="rememberme" value="forever" type="checkbox"><span>Souviens-toi de moi</span>
+										</label>
+										<a class="link-function left-position" href="{{route('password.request')}}" title="Forgotten password?">Mot de passe oublié ?</a>
+									</fieldset>
+									<input type="submit" class="btn btn-submit" value="Login" name="submit">
+								</form>
+							</div>												
+						</div>
+					</div><!--end main products area-->		
+				</div>
+			</div><!--end row-->
+
+		</div><!--end container-->
+
+	</main>
+
 </x-guest-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
@@ -57,4 +57,65 @@
             </div>
         </form>
     </x-jet-authentication-card>
+</x-guest-layout> --}}
+
+
+<x-guest-layout>
+
+    <main id="main" class="main-site left-sidebar">
+
+		<div class="container">
+
+			<div class="wrap-breadcrumb">
+				<ul>
+					<li class="item-link"><a href="/" class="link">Accueil</a></li>
+					<li class="item-link"><span>Inscription</span></li>
+				</ul>
+			</div>
+			<div class="row">
+				<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12 col-md-offset-3">							
+					<div class=" main-content-area">
+						<div class="wrap-login-item ">
+							<div class="register-form form-item ">
+
+                                <x-jet-validation-errors class="mb-4" />
+
+								<form class="form-stl" action="{{route('register')}}" name="frm-login" method="POST" >
+									@csrf
+                                    
+                                    <fieldset class="wrap-title">
+										<h3 class="form-title">Crèer un compte</h3>
+										<h4 class="form-subtitle">Infomations personnelles</h4>
+									</fieldset>									
+									<fieldset class="wrap-input">
+										<label for="frm-reg-lname">Nom complet*</label>
+										<input type="text" id="frm-reg-lname" name="name" placeholder="Votre nom complet*" :value="old('name')" required autofocus autocomplete="name" >
+									</fieldset>
+									<fieldset class="wrap-input">
+										<label for="frm-reg-email">Email Address*</label>
+										<input type="email" id="frm-reg-email" name="email" placeholder="Email address" :value="old('email')">
+									</fieldset>
+                                    <fieldset class="wrap-title">
+										<h3 class="form-title">Informations de connexion</h3>
+									</fieldset>
+									<fieldset class="wrap-input item-width-in-half left-item ">
+										<label for="frm-reg-pass">Mot de passe *</label>
+										<input type="password" id="frm-reg-pass" name="password" placeholder="Password" autocomplete="new-password" required>
+									</fieldset>
+									<fieldset class="wrap-input item-width-in-half ">
+										<label for="frm-reg-cfpass">Confirmer le mot de passe *</label>
+										<input type="password" id="frm-reg-cfpass" name="password_confirmation" placeholder="Confirmer votre mot de passe" required autocomplete="new-password">
+									</fieldset>
+									<input type="submit" class="btn btn-sign" value="Register" name="register">
+								</form>
+							</div>											
+						</div>
+					</div><!--end main products area-->		
+				</div>
+			</div><!--end row-->
+
+		</div><!--end container-->
+
+	</main>
+
 </x-guest-layout>
